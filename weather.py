ecb904f2fb14fbb5cd1ea39412f9d47eb84e6ddb
mbbartnik <mbbartnik@gmail.com>
2018-09-18 16:30:13 +0200

    fg

--- weather.py
+++ weather.py
@@ -65,9 +65,9 @@ def processRequest(req):
             speech = "Hello Michael, today we will talk about sience!"
 
         elif name in ("Filip", "Philip"):
-            speech = "Hello Filip, did you miss me??"
+            speech = "Hello" + name +", did you miss me??"
 
-        elif name == ("Markus","Marcus"):
+        elif name in ("Markus","Marcus"):
             speech = "Hello my owner! Yes these is me, your robot Pepper!"
         else :
             namedropping = name
